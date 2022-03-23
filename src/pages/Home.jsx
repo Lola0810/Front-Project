4281261f6af661558da481277980275768e6b98d
Mattéo <40558258+Matteo0810@users.noreply.github.com>
2022-03-23 21:22:42 +0100

    je suis pas passé par là
    je me contente de déposer les fichiers de lola, j'ai rien écrit de +

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,36 +8,48 @@ export default class Home extends Component {
                 <div className="wave__1" />
                 <div className="wave__2" />
 
-                <h1 className="title">CLASH OF CODE</h1>
+                <div className="presentation">
+                    <div className="left">
+                        <h1 className="title">CLASH OF CODE</h1>
+                        <h2 className="description">
+                            Amuse toi avec tes amis, créer des parties en local et
+                            invite tous tes amis. <br />
+                            Jouable de n'importe où avec une connexion ! Surtout 
+                            en cours de Philosophie.
+                        </h2>
+
+                        <button className="create">Créer</button>
+                        <button className="join">Rejoindre</button>
+                    </div>
+                    <div className="front__image" />
+                </div>
+
+                <div className="goal">
+                    <h1 className="title">Quel est le but du jeu ?</h1>
+
+                    <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Duis maximus ex ac nunc mollis lobortis. Aenean sollicitudin lectus ligula.
+                    Vivamus venenatis sed ipsum at tristique. Ut varius arcu eget diam aliquam suscipit. 
+                    Vivamus ut purus viverra, pellentesque arcu fermentum, pulvinar nunc. 
+                    Sed consequat nisl in tristique finibus. In varius tellus in diam accumsan mattis. 
+                    Curabitur venenatis ut augue et laoreet. Ut mollis pellentesque sem, vitae ullamcorper orci tempor at.
+                    Donec non mauris non lacus fringilla ornare quis eget augue.</p>
+                </div>
                 
-                <p className="description">
-                    Amuse toi avec tes amis, créer des parties en local et
-                    invite tous tes amis. <br></br>
-                    Jouable de n'importe où avec une connexion ! Surtout 
-                    en cours de Philosophie.
-                </p>
-
-                <button className="create">Créer</button>
-                <button className="join">Rejoindre</button>
-
-                <div className="front__image" />
-
-                <h1 className="goal__title">Quel est le but du jeu ?</h1>
-
-                <p className="goal__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Duis maximus ex ac nunc mollis lobortis. Aenean sollicitudin lectus ligula.
-                Vivamus venenatis sed ipsum at tristique. Ut varius arcu eget diam aliquam suscipit. 
-                Vivamus ut purus viverra, pellentesque arcu fermentum, pulvinar nunc. 
-                Sed consequat nisl in tristique finibus. In varius tellus in diam accumsan mattis. 
-                Curabitur venenatis ut augue et laoreet. Ut mollis pellentesque sem, vitae ullamcorper orci tempor at.
-                Donec non mauris non lacus fringilla ornare quis eget augue.</p>
                 
-                
-                <div className="down__main"/>
-                <h2 className="greensubtitle__1">Bats toi avec tes amis</h2>
-                <h2 className="greensubtitle__2">pour être le premier à finir !</h2>
-                <h2 className="pinksubtitle__1">Essaie de les frainer</h2>
-                <h2 className="pinksubtitle__1">le plus possible !</h2>
+                <div className="deeper__background">
+
+                    <div className="subtitle green">
+                        <h2>Bats toi avec tes amis</h2>
+                        <h2>pour être le premier à finir !</h2>
+                    </div>
+
+                    <div className="subtitle pink">
+                        <h2>Essaie de les frainer</h2>
+                        <h2>le plus possible !</h2>
+                    </div>
+
+                </div>
             </section>
         )
     }
