59ffb01580ad1f17ae6c812fc7b6493a2b5ec0bb
Thumus <thumus@tutanota.com>
2022-04-13 20:55:48 +0200

    Test

--- src/public/api.js
+++ src/public/api.js
@@ -1,14 +1,47 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "https://clashofcode.api.fr/",
+    baseURL: "https://inchallahOnAuraUnDomaine/api/v1",
     headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`
+        Authorization: `Bearer ${getUserToken()}`
     }
 })
-const USER = 'users'
+
+/* TYPES */
+const USER = 'user'
+const ROOM = 'room'
+const CHAT = 'chat'
+
+/* TOKEN */
+const storeUserToken = (userToken) => localStorage.setItem('token', userToken)
+const getUserToken = () => localStorage.getItem('token')
+const changeToken = () => api.config.headers["Authorisation"] = `Bearer ${getUserToken()}`
 
 /* USER */
-const storeUserToken = (userToken) => localStorage.setItem('token', userToken)
-const getUserData = api.get(`${USER}/get`)
-//.....
+const getUserMe = () => api.get(`${USER}/@me`);
+const getUserData = (user) => api.get(`${USER}/paLaRoute/${user}`);
+const loginUser = ({username, password}) => api.post(`${USER}/login`, {username, password});
+const siginUser = ({username, password, email}) => api.post(`${USER}/sigin`, {username, password, email});
+const classementUser = (type) => api.get(`${USER}/classement/${type}`);
+
+/* ROOM */
+
+
+
+const Token = {
+    storeUserToken,
+    getUserToken,
+    changeToken
+};
+
+const User = {
+    getUserMe,
+    getUserData,
+    loginUser,
+    siginUser,
+    classementUser
+}
+
+export {
+    
+}
