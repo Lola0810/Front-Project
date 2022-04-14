21bda7fac2e695b618c82bfbd198ca9788d11a20
Thumus <thumus@tutanota.com>
2022-04-15 00:51:37 +0200

    a

--- src/public/api.js
+++ src/public/api.js
@@ -1,47 +1,47 @@
-import axios from 'axios'
+import axios from "axios";
 
-const api = axios.create({
+const api = () =>
+  axios.create({
     baseURL: "https://inchallahOnAuraUnDomaine/api/v1",
     headers: {
-        Authorization: `Bearer ${getUserToken()}`
-    }
-})
+      Authorization: `Bearer ${getUserToken()}`,
+    },
+  });
 
 /* TYPES */
-const USER = 'user'
-const ROOM = 'room'
-const CHAT = 'chat'
+const USER = "user";
+const ROOM = "room";
+const CHAT = "chat";
 
 /* TOKEN */
-const storeUserToken = (userToken) => localStorage.setItem('token', userToken)
-const getUserToken = () => localStorage.getItem('token')
-const changeToken = () => api.config.headers["Authorisation"] = `Bearer ${getUserToken()}`
+const storeUserToken = (userToken) => localStorage.setItem("token", userToken);
+const getUserToken = () => localStorage.getItem("token");
+const changeToken = () =>
+  (api.config.headers["Authorisation"] = `Bearer ${getUserToken()}`);
 
 /* USER */
 const getUserMe = () => api.get(`${USER}/@me`);
 const getUserData = (user) => api.get(`${USER}/paLaRoute/${user}`);
-const loginUser = ({username, password}) => api.post(`${USER}/login`, {username, password});
-const siginUser = ({username, password, email}) => api.post(`${USER}/sigin`, {username, password, email});
-const classementUser = (type) => api.get(`${USER}/classement/${type}`);
+const loginUser = ({ username, password }) =>
+  api.post(`${USER}/login`, { username, password });
+const siginUser = ({ username, password, email }) =>
+  api.post(`${USER}/sigin`, { username, password, email });
+const leaderboardUser = (type) => api.get(`${USER}/leaderboard/${type}`);
 
 /* ROOM */
 
-
-
 const Token = {
-    storeUserToken,
-    getUserToken,
-    changeToken
+  storeUserToken,
+  getUserToken,
+  changeToken,
 };
 
 const User = {
-    getUserMe,
-    getUserData,
-    loginUser,
-    siginUser,
-    classementUser
-}
+  getUserMe,
+  getUserData,
+  loginUser,
+  siginUser,
+  leaderboardUser,
+};
 
-export {
-    
-}
+export { Token, User, api };
